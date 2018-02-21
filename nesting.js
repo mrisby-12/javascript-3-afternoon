02f6c969a806d3db210cba8eca9c046ccab1943c
unknown <michellenrisby@gmail.com>
2018-02-21 14:56:35 -0600

    JS-3-completed

--- nesting.js
+++ nesting.js
@@ -50,7 +50,16 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
+function employeeUpdater() {
+  for (let i = 0; i < employees.length; i++) {
+    if (employees[i].firstName === 'Theo') {
+      employees.splice(i, 1);
+    } else if(employees[i].firstName === 'Lorie') {
+      employees[i].department = 'HR';
+    }
+  }
+  return employees;
+}
 
 
 
@@ -68,7 +77,17 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
+function removeDuplicates() {
+  for (let i = 0; i < workplaceAccidents.length; i++) {
+    for( let j = 0; j < workplaceAccidents.length; j++) {
+      if(workplaceAccidents[i] === workplaceAccidents[j] && i !== j) {
+        workplaceAccidents.splice(j,1);
+        i--;
+      }
+    }
+  }
+  return workplaceAccidents;
+}
 
 
 
@@ -97,8 +116,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
@@ -138,7 +157,11 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
+function recordCleaner() {
+  for ( let i = 0; i < myCar.accidents.length; i++) {
+    myCar.accidents[i].atFaultForAccident = false;
+  }
+}
 
 
 
@@ -157,6 +180,13 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
+function looper() {
+  for (let i = 0; i <numsArr.length; i++) {
+    for ( let j = 0; j < numsArr[i].length; j++) {
+      numsArr[i][j] % 2 === 0 ? numsArr[i].splice(j, 1, 'even') : numsArr[i].splice(j, 1, 'odd');
+    }
+  }
+  return numsArr;
+}
 
 
